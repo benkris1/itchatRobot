9a2ac3a84f68c14725562af29c167f5f8b6deb2d
马传林 <cma@maxleap.com>
2017-05-24 10:36:42 +0800

    fxi

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 WORKING_DIR=/opt/itchatRobot
-docker run --name itchatRobot --net="host" -v $WORKING_DIR/logs:/opt/itchatRobot/logs -d itchatRobot:0.1
+docker run --name itchatRobot --net="host" -v $WORKING_DIR/logs:/opt/itchatRobot/logs -d itchat_robot:0.1
